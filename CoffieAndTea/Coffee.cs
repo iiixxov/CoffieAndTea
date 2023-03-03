50e00ef4adeb2b835d7dfae653c946b781d3ca1f
veligzhanin.2020 <iiixxov@gmail.com>
2023-03-03 14:14:08 +0700

    Добавьте файлы проекта.

---
+++ CoffieAndTea/Coffee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i02_1_TemplateMethod
+{
+    class Coffee
+    {
+        public void prepareRecipe()
+        {
+            boilWater();
+            brewCoffeeGrinds();
+            pourInCup();
+            addSugarAndMilk();
+        }
+
+        private void addSugarAndMilk()
+        {
+            Console.WriteLine("Adding Sugar and Milk");
+        }
+
+        private void pourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        private void brewCoffeeGrinds()
+        {
+            Console.WriteLine("Dripping coffee through filter");
+        }
+
+        private void boilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+    }
+}
---
+++ CoffieAndTea/Tea.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace i02_1_TemplateMethod
+{
+    class Tea
+    {
+        public void prepareRecipe()
+        {
+            boilWater();
+            steepTeaBag();
+            pourInCup();
+            addLemon();
+        }
+
+        private void addLemon()
+        {
+            Console.WriteLine("Adding lemon");
+        }
+
+        private void pourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+
+        private void steepTeaBag()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+
+        private void boilWater()
+        {
+            Console.WriteLine("Boiling water");
+        }
+    }
+}
